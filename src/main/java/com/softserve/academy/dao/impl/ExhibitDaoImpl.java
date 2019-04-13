b170b65d6ff63bf0eee8b4fea06acbfb5b71827d
Quadhands <dimanarepeha@gmail.com>
2019-04-13 18:27:39 +0300

    Add option to write new exhibit

--- src/main/java/com/softserve/academy/dao/impl/ExhibitDaoImpl.java
+++ src/main/java/com/softserve/academy/dao/impl/ExhibitDaoImpl.java
@@ -14,7 +14,115 @@ import java.util.List;
 public class ExhibitDaoImpl implements ExhibitDao {
     @Override
     public boolean saveExhibit(ExhibitEntity exhibit) {
-        return false;
+        try (PreparedStatement insertToExhibit = Database.getInstance()
+                .getConnection()
+                .prepareStatement("INSERT INTO exhibit(id_material,id_technique,id_hall,exhibit_name)" +
+                        "VALUES(?,?,?,?)");
+             PreparedStatement checkAuthor = Database.getInstance()
+                     .getConnection()
+                     .prepareStatement("SELECT id_author FROM author WHERE FIRSTNAME=? AND LASTNAME=?");
+             PreparedStatement addAuthor = Database.getInstance()
+                     .getConnection()
+                     .prepareStatement("INSERT INTO author(FIRSTNAME,LASTNAME)" +
+                             "VALUES(?,?)");
+             PreparedStatement checkMaterial = Database.getInstance()
+                     .getConnection()
+                     .prepareStatement("SELECT id_material FROM material WHERE material_name=?");
+             PreparedStatement addMaterial = Database.getInstance()
+                     .getConnection()
+                     .prepareStatement("INSERT INTO material(material_name)" +
+                             "VALUES(?)");
+             PreparedStatement checkHall = Database.getInstance()
+                     .getConnection()
+                     .prepareStatement("SELECT id_hall FROM hall WHERE hall_name=?");
+             PreparedStatement addHall = Database.getInstance()
+                     .getConnection()
+                     .prepareStatement("INSERT INTO hall(hall_name)" +
+                             "VALUES(?)");
+             PreparedStatement checkTechnique = Database.getInstance()
+                     .getConnection()
+                     .prepareStatement("SELECT id_technique FROM technique WHERE technique_name=?");
+             PreparedStatement addTechnique = Database.getInstance()
+                     .getConnection()
+                     .prepareStatement("INSERT INTO technique(technique_name)" +
+                             "VALUES(?)");
+             PreparedStatement insertToAuthor_Exhibit = Database.getInstance()
+                     .getConnection()
+                     .prepareStatement("INSERT INTO author_exhibit(id_exhibit,id_author)" +
+                             "VALUES(?,?)");
+             PreparedStatement selectFromExhibitLast = Database.getInstance()
+                     .getConnection()
+                     .prepareStatement("SELECT id_exhibit FROM exhibit ORDER BY id_exhibit DESC LIMIT 1 ")) {
+            /**check all and add all elements that are not in database*/
+            checkAuthor.setString(1, exhibit.getFirstName());
+            checkAuthor.setString(2, exhibit.getLastName());
+            ResultSet resultSet = checkAuthor.executeQuery();
+            if (!resultSet.first()) {//then add
+                addAuthor.setString(1, exhibit.getFirstName());
+                addAuthor.setString(2, exhibit.getLastName());
+                addAuthor.execute();
+            }
+            resultSet.close();
+            resultSet = checkAuthor.executeQuery();
+            resultSet.first();
+            int authorId = resultSet.getInt(1);
+
+            checkHall.setString(1, exhibit.getHall_name());
+            resultSet = checkHall.executeQuery();
+            if (!resultSet.first()) {//then add
+                addHall.setString(1, exhibit.getHall_name());
+                addHall.execute();
+            }
+            resultSet.close();
+            resultSet = checkHall.executeQuery();
+            resultSet.first();
+            int hallId = resultSet.getInt(1);
+
+            checkMaterial.setString(1, exhibit.getMaterial_name());
+            resultSet = checkMaterial.executeQuery();
+            if (!resultSet.first()) {//then add
+                addMaterial.setString(1, exhibit.getMaterial_name());
+                addMaterial.execute();
+            }
+            resultSet.close();
+            resultSet = checkMaterial.executeQuery();
+            resultSet.first();
+            int materialId = resultSet.getInt(1);
+
+            checkTechnique.setString(1, exhibit.getTechnique_name());
+            resultSet = checkTechnique.executeQuery();
+            if (!resultSet.first()) {//then add
+                addTechnique.setString(1, exhibit.getTechnique_name());
+                addTechnique.execute();
+            }
+            resultSet.close();
+            resultSet = checkTechnique.executeQuery();
+            resultSet.first();
+            int techniqueId = resultSet.getInt(1);
+
+            String exhibitName = exhibit.getExhibit_name();
+
+            insertToExhibit.setInt(1, materialId);
+            insertToExhibit.setInt(2, techniqueId);
+            insertToExhibit.setInt(3, hallId);
+            insertToExhibit.setString(4, exhibitName);
+            insertToExhibit.execute();
+            resultSet = selectFromExhibitLast.executeQuery();
+            resultSet.first();
+            int idOfNewExhibit = resultSet.getInt(1);
+            resultSet.close();
+            insertToAuthor_Exhibit.setInt(1, idOfNewExhibit);
+            insertToAuthor_Exhibit.setInt(2, authorId);
+            insertToAuthor_Exhibit.execute();
+            System.out.println("Successfully added new exhibit " + exhibitName);
+            return true;
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
@@ -43,6 +151,7 @@ public class ExhibitDaoImpl implements ExhibitDao {
             resultSet.close();
         } catch (SQLException e) {
             e.printStackTrace();
+            return result;
         }
         return result;
     }
@@ -63,9 +172,10 @@ public class ExhibitDaoImpl implements ExhibitDao {
             return rowsAffected;
         } catch (NumberFormatException e) {
             System.out.println("Maybe you have entered letters or space somewhere");
+            return 0;
         } catch (SQLException e) {
             e.printStackTrace();
+            return 0;
         }
-        return 0;
     }
 }
--- src/main/java/com/softserve/academy/servlets/AddExhibitServlet.java
+++ src/main/java/com/softserve/academy/servlets/AddExhibitServlet.java
@@ -1,8 +1,55 @@
 package com.softserve.academy.servlets;
 
+import com.softserve.academy.dao.ExhibitDao;
+import com.softserve.academy.dao.impl.ExhibitDaoImpl;
+import com.softserve.academy.dao.impl.GuideDaoImpl;
+import com.softserve.academy.entity.ExhibitEntity;
+import com.softserve.academy.entity.GuideEntity;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class AddExhibitServlet extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ExhibitDao exhibitDao = new ExhibitDaoImpl();
+        req.setAttribute("exhibits", exhibitDao.readAllExhibits());
+        req.getRequestDispatcher("addExhibit.jsp").forward(req, resp);
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ExhibitEntity exhibitEntity = new ExhibitEntity(0);
+        ExhibitDao exhibitDao = new ExhibitDaoImpl();
+        String exhibitName = req.getParameter("exhibitName");
+        String firstname = req.getParameter("firstname");
+        String lastname = req.getParameter("lastname");
+        String hall = req.getParameter("hall");
+        String material = req.getParameter("material");
+        String technique = req.getParameter("technique");
+        exhibitEntity.setExhibit_name(exhibitName);
+        exhibitEntity.setFirstName(firstname);
+        exhibitEntity.setLastName(lastname);
+        exhibitEntity.setMaterial_name(material);
+        exhibitEntity.setTechnique_name(technique);
+        exhibitEntity.setHall_name(hall);
+        if (!exhibitName.equals("")&&!firstname.equals("")
+                &&!lastname.equals("")&&!hall.equals("")
+                &&!material.equals("")&&!technique.equals("")
+                &&exhibitDao.saveExhibit(exhibitEntity)) {
+            req.setAttribute("success", 1);
+            System.out.println("Success = 1");
+        } else {
+            System.out.println("Success = 0");
+            req.setAttribute("success", 0);
+        }
+
+        doGet(req, resp);
+    }
 }
