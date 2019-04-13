5bf5a36a5dff1461418c45bce9f1cdc5b88cbdc2
Quadhands <dimanarepeha@gmail.com>
2019-04-13 13:05:50 +0300

    Add ListExhibitServlet as class and to web xml

---
+++ src/main/java/com/softserve/academy/servlets/ExhibitListServlet.java
@@ -0,0 +1,15 @@
+package com.softserve.academy.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ExhibitListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
