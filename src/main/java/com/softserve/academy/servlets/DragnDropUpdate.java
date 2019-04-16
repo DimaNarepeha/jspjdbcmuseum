3cd459fc08e8050c0351581ad8927d6d18da8510
Quadhands <dimanarepeha@gmail.com>
2019-04-16 20:50:25 +0300

    Add servlet to web xml

--- src/main/java/com/softserve/academy/servlets/DragnDropUpdate.java
+++ src/main/java/com/softserve/academy/servlets/DragnDropUpdate.java
@@ -4,24 +4,32 @@ import com.softserve.academy.dao.ExhibitDao;
 import com.softserve.academy.dao.ExhibitGuideDao;
 import com.softserve.academy.dao.impl.ExhibitDaoImpl;
 import com.softserve.academy.dao.impl.ExhibitGuideDaoImpl;
-import com.softserve.academy.entity.ExhibitEntity;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DragnDropUpdate extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ExhibitDao exhibitDao = new ExhibitDaoImpl();
+        req.setAttribute("exhibits", exhibitDao.readAllExhibits());
+        req.getRequestDispatcher("ListExhibitWithButtons.jsp");
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ExhibitGuideDao exhibitGuideDao = new ExhibitGuideDaoImpl();
-        req.setAttribute("exhibit", exhibitGuideDao.getExhibitsByGuideId());
+        req.setAttribute("exhibit", exhibitGuideDao.getExhibitsByGuideId(Integer.parseInt(req.getParameter("idExhibit"))));
         req.getRequestDispatcher("addExhibit.jsp").forward(req, resp);
-        if(req.getParameter("idsToUpdate")!=null){
-
+        if (req.getParameter("idsToUpdate") != null) {
+            HashSet<Integer> ids = (HashSet<Integer>) Arrays.asList(req.getParameter("idsToUpdate").split(" ")).stream().map(Integer::parseInt).collect(Collectors.toList());
         }
         req.getRequestDispatcher("dragndropaddguideexhibit.jsp");
     }
