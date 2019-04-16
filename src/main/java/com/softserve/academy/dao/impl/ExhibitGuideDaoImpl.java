65ef0ba6eb3dfa62284e8cdd6faeec700643181d
Quadhands <dimanarepeha@gmail.com>
2019-04-16 19:48:05 +0300

    add exhibitguideimpl

---
+++ src/main/java/com/softserve/academy/dao/impl/ExhibitGuideDaoImpl.java
@@ -0,0 +1,19 @@
+package com.softserve.academy.dao.impl;
+
+import com.softserve.academy.dao.ExhibitGuideDao;
+import com.softserve.academy.entity.ExhibitEntity;
+import com.softserve.academy.entity.GuideEntity;
+
+import java.util.List;
+
+public class ExhibitGuideDaoImpl implements ExhibitGuideDao {
+    @Override
+    public List<GuideEntity> getGuidesByExhibitId() {
+        return null;
+    }
+
+    @Override
+    public List<ExhibitEntity> getExhibitsByGudeId() {
+        return null;
+    }
+}
